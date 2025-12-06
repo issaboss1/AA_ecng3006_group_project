7efac565ce9ad431983f47d028c07cf7cc1a1e3f
issaboss1 <issa.mohamed@my.uwi.edu>
2025-12-06 17:36:50 -0400

    Add files via upload

---
+++ RTC/RTC/rtc/user_main.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+#include <ds3231.h>
+#include <string.h>
+#define CONFIG_EXAMPLE_I2C_MASTER_SDA 2
+#define CONFIG_EXAMPLE_I2C_MASTER_SCL 0
+
+
+
+void ds3231_test(void *pvParameters)
+{
+    i2c_dev_t dev;
+    memset(&dev, 0, sizeof(i2c_dev_t));
+
+    ESP_ERROR_CHECK(ds3231_init_desc(&dev, 0, CONFIG_EXAMPLE_I2C_MASTER_SDA, CONFIG_EXAMPLE_I2C_MASTER_SCL));
+
+    // setup datetime: 2016-10-09 13:50:10
+    struct tm time = {
+        .tm_year = 125, //since 1900 (2016 - 1900)
+        .tm_mon  = 11,  // 0-based
+        .tm_mday = 21,
+        .tm_hour = 13,
+        .tm_min  = 50,
+        .tm_sec  = 10
+    };
+    
+    ESP_ERROR_CHECK(ds3231_set_time(&dev, &time));
+
+    ds3231_enable_squarewave(&dev);// enable the square wave
+    ds3231_set_squarewave_freq(&dev,DS3231_SQWAVE_4096HZ);// set the frequency 
+    //ds3231_enable_32khz(&dev);//32k output
+    ESP_ERROR_CHECK(ds3231_enable_squarewave(&dev));
+    ESP_ERROR_CHECK(ds3231_set_squarewave_freq(&dev,DS3231_SQWAVE_8192HZ));
+    
+    while (1)
+    {
+        float temp;
+
+        vTaskDelay(pdMS_TO_TICKS(500));//delay
+
+        if (ds3231_get_temp_float(&dev, &temp) != ESP_OK)
+        {
+            printf("Could not get temperature\n");
+            continue;
+        }
+        
+        if (ds3231_get_time(&dev, &time) != ESP_OK)
+        {
+            printf("Could not get time\n");
+            continue;
+        }
+       
+        /* float is used in printf(). you need non-default configuration in
+         * sdkconfig for ESP8266, which is enabled by default for this
+         * example. see sdkconfig.defaults.esp8266
+         */
+        printf("%04d-%02d-%02d %02d:%02d:%02d, %.2f deg Cel\n", time.tm_year + 1900 /*Add 1900 for better readability*/, time.tm_mon + 1,
+            time.tm_mday, time.tm_hour, time.tm_min, time.tm_sec, temp);
+    }
+}
+
+void app_main()
+{
+    ESP_ERROR_CHECK(i2cdev_init());
+    xTaskCreate(ds3231_test, "ds3231_test", configMINIMAL_STACK_SIZE * 3, NULL, 5, NULL);
+}
